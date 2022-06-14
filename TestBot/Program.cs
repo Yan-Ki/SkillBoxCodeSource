c8481752a84b9d20096a480e35b3f4e27d6fab3d
Alex <Alex@DESKTOP-PMANOV5>
2022-06-14 19:30:02 +0400

    Добавьте файлы проекта.

---
+++ TestBot/Program.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+
+
+//if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+//{
+//    var message = update.Message;
+//    switch (message.Text.ToLower())
+//    {
+//        case "/start":
+//            await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
+//            break;
+//        case "hi":
+//            await botClient.SendTextMessageAsync(message.Chat, "Да пошёл ты!!!");
+//            break;
+//        default:
+//            await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
+//            break;
+//    }
+
+namespace TestBot
+{
+    class Program
+    {
+        private static string token { get; set; } = "ВСТАВЬТЕ СЮДА СВОЙ ТОКЕН!";
+        private static TelegramBotClient Bot;
+        static void Main(string[] args)
+        {
+            Bot = new TelegramBotClient(token);
+
+            //using var cts = new CancellationTokenSource();
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            //ReceiverOptions receiverOptions = new() { AllowedUpdates = { } };
+            //Bot.StartReceiving(Handlers.HandleUpdateAsync,
+            //                   Handlers.HandleErrorAsync,
+            //                   receiverOptions,
+            //                   cts.Token);
+
+            //Console.WriteLine($"Бот запущен и ждет сообщения...");
+            //Console.ReadLine();
+
+            //// Send cancellation request to stop bot
+            //cts.Cancel();
+        }
+    }
+
+}
+
---
+++ Testbot2/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Testbot2
+{
+    internal class Program
+    {
+        //string url = @"data-26099-2022-05-23.zip";
+        //WebClient client = new WebClient() { Encoding = Encoding.Default };
+        //client.BaseAddress = "https://data.mos.ru/";
+        //    client.DownloadFile(url, "data-26099-2022-05-23.zip");
+        //5512214364:AAGjH5_zeRw0uYRhEGug5Tv87SCD2FnDHG8
+        static void Main(string[] args)
+        {
+            string token = "5512214364:AAGjH5_zeRw0uYRhEGug5Tv87SCD2FnDHG8";
+            WebClient wc = new WebClient() { Encoding = Encoding.Default };
+            string startUrl = $@"https://api.telegram.org/bot{token}/";
+
+            int update_id = 0;
+            while (true)
+            {
+                string url = $"{startUrl}getUpdates?offset={update_id}";
+                string r = wc.DownloadString(url);
+                Console.WriteLine(r);
+                //Console.ReadKey();
+                JToken[] mass = JObject.Parse(r)["result"].ToArray();
+                foreach (dynamic m in mass)
+                {
+                    update_id = Convert.ToInt32(m.update_id) + 1;
+                    string userMessage = m["message"]["text"].ToString();
+                    string userId = m.message.from.id;
+                    string useFirstrName = m.message.from.first_name;
+
+                    string text = $"{useFirstrName} {userId} {userMessage}";
+
+                    Console.WriteLine(text);
+                    if (userMessage == "hi")
+                    {
+                        string responseText = $"Здравствуй ушлёпок, {useFirstrName}";
+                        url = $"{startUrl}sendMessage?chat_id={userId}&text={responseText}";
+                        //Console.WriteLine("+");
+                        Console.WriteLine(wc.DownloadString(url));
+                    }
+                }
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
